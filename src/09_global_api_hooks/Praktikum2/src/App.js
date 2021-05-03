2f186df6c3deb9fe21ab55881aefe66a5b28a844
IkaLailatuzzahro <ikalailatuzzahrojoe@gmail.com>
2021-05-03 10:01:19 +0700

    09

---
+++ src/09_global_api_hooks/Praktikum2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BlogPost from './components/BlogPost';
+
+class App extends React.Component {
+
+    render() {
+        return ( <
+            div >
+            <
+            BlogPost / >
+            <
+            /div>
+        );
+    }
+}
+
+export default App;
