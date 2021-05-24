3dd61522c50ed0ac83ba47c9a9fea90f9b975ae6
IkaLailatuzzahro <ikalailatuzzahrojoe@gmail.com>
2021-05-24 11:34:51 +0700

    10

---
+++ src/10_Firebase_ReactJS/praktikum 3/src/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+        apiKey: "AIzaSyAcpXY7Xy2BWPI_670CyEuoHqSimmplJEI",
+        authDomain: "belajar-react-27452.firebaseapp.com",
+        projectId: "belajar-react-27452",
+        storageBucket: "belajar-react-27452.appspot.com",
+        messagingSenderId: "857691583081",
+        appId: "1:857691583081:web:780d4e37f277c4fc5d68f8",
+        measurementId: "G-4J3RBGV1K5"
+      };
